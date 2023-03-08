dc0794f6323a29de4c32c359f335b9f8ffda5a44
Hà Ngọc Minh <hangoc.minh1306@gmail.com>
2023-03-08 12:33:41 +0700

    update theme switcher button style
    Correctly active color shown for each theme.

--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -2,9 +2,13 @@ import { SunIcon } from "@heroicons/react/24/solid";
 import { MoonIcon } from "@heroicons/react/24/solid";
 import { ComputerDesktopIcon } from "@heroicons/react/24/solid";
 import { useTheme } from "../contexts/ThemeContext";
+import { useState } from "react";
 
 export function ThemeSwitcher({ expandTheme, setExpandTheme }) {
   const { theme, setTheme, getTheme } = useTheme();
+  const [storageTheme, setStorageTheme] = useState(
+    localStorage.getItem("theme") ? true : false
+  );
 
   return (
     <div
@@ -15,39 +19,54 @@ export function ThemeSwitcher({ expandTheme, setExpandTheme }) {
         setExpandTheme((prev) => !prev);
       }}
     >
-      <div className="flex items-center justify-center gap-1 px-1">
+      <button className="flex items-center justify-center gap-1 px-1">
         <ComputerDesktopIcon className="h-4 w-4 text-white" />
         <p className="font-bold text-white">Theme</p>
-      </div>
+      </button>
       <div
         className={`${
           expandTheme ? "block" : "hidden"
         } absolute top-8 left-[-50%] flex flex-col items-center justify-center gap-1 rounded bg-black p-3 shadow-lg shadow-slate-700`}
       >
-        <div
+        <button
           className={`flex h-9 w-40 items-center gap-3 rounded ${
-            !theme ? "bg-gray-700" : ""
+            !storageTheme ? "bg-gray-700" : ""
           } p-2 shadow-sm hover:bg-gray-400`}
+          onClick={() => {
+            localStorage.removeItem("theme");
+            setTheme(getTheme());
+            setStorageTheme(false);
+          }}
         >
           <ComputerDesktopIcon className="h-5 w-5 text-white" />
           <p className="text-sm font-bold text-white">Default</p>
-        </div>
-        <div
+        </button>
+        <button
           className={`flex h-9 w-40 items-center gap-3 rounded ${
-            theme === "light" ? "bg-gray-700" : ""
+            theme === "light" && storageTheme ? "bg-gray-700" : ""
           } p-2 shadow-sm hover:bg-gray-400`}
+          onClick={() => {
+            localStorage.setItem("theme", "light");
+            setTheme(getTheme());
+            setStorageTheme(true);
+          }}
         >
           <SunIcon className="h-5 w-5 text-white" />
           <p className="text-sm font-bold text-white">Light</p>
-        </div>
-        <div
+        </button>
+        <button
           className={`flex h-9 w-40 items-center gap-3 rounded ${
-            theme === "dark" ? "bg-gray-700" : ""
+            theme === "dark" && storageTheme ? "bg-gray-700" : ""
           } p-2 shadow-sm hover:bg-gray-400`}
+          onClick={() => {
+            localStorage.setItem("theme", "dark");
+            setTheme(getTheme());
+            setStorageTheme(true);
+          }}
         >
           <MoonIcon className="h-5 w-5 text-white" />
           <p className="text-sm font-bold text-white">Dark</p>
-        </div>
+        </button>
       </div>
     </div>
   );
